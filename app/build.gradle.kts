86dcbff4f98fe50df1eac9ffcb1d0c5205bde7ec
Zijun Li <zjli@terpmail.umd.edu>
2025-05-12 13:34:43 -0400

    fixed the gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.gms.google-services") version "4.4.1"
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
